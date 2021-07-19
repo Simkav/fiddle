7b6cf6bb1cc152bde5d1b9361ff4049c68a8eed7
Ivan Khomenko <simkav2411@gmail.com>
2021-07-19 15:15:24 +0300

    add fs utils

---
+++ src/utils/fsutil.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const fs = require('fs')
+const defaultRoomLayotDir = path.resolve(__dirname, '../', 'defaultRoom')
+const roomsPath = path.resolve(__dirname, '../', 'public', 'rooms')
+const defaultFiles = ['index.html', 'styles.css', 'index.js']
+const parseFile = async (nickname, file) =>
+  await fs.promises.readFile(path.resolve(roomsPath, nickname, file), {
+    encoding: 'utf8'
+  })
+
+const parseRoomFiles = async nickname =>
+  Promise.all(defaultFiles.map(file => parseFile(nickname, file))).then(
+    data => data
+  )
+
+const copyDefaultRoom = dir =>
+  defaultFiles.forEach(file => {
+    fs.copyFile(
+      path.join(defaultRoomLayotDir, file),
+      path.join(dir, file),
+      err => {
+        if (err) console.error(err)
+      }
+    )
+  })
+
+const createRoom = nickname => {
+  const dir = path.join(roomsPath, nickname)
+  fs.access(dir, err => {
+    if (err) {
+      fs.mkdir(dir, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        copyDefaultRoom(dir)
+      })
+    }
+  })
+}
+
+module.exports = {
+  createRoom,
+  parseRoomFiles,
+  parseFile
+}
