a18ede46637c2d26fc2fbf04954ad0420dcce929
Ivan Khomenko <simkav2411@gmail.com>
2021-07-20 12:43:41 +0300

    update scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
-  mode: 'development',
   entry: {
     main: path.resolve(__dirname, 'src/client/index.js')
   },
